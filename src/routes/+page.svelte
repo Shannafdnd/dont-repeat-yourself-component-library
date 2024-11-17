649c7aff97a7570b0c96228b25a16748f74cabb3
Shanna van der Werve <shanna.van.der.werve@hva.nl>
2024-11-17 22:54:29 +0100

    Zoekbalk werkt

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { HelloWorld, SearchBar } from "@shannafdnd/fdnd-components";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<HelloWorld />
+
+<div class="wrapper">
+    <SearchBar placeholder="Zoeken..." resultsPage="/zoeken" name="searchTerm" />
+</div>
+
+<style>
+    .wrapper {
+        width: 50%;
+        display: flex;
+        justify-content: end;
+    }
+</style>
